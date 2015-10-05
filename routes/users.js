b21a9f2a32ef7074c81e139e2fc4ab182bb06e5c
ha1fha1f <half.ldk.hiz@gmail.com>
2015-10-06 00:48:16 +0900

    文言を修正、userの方を変更

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:name?', function(req, res, next) {
+    if (req.params.name) {
+        res.send(
+            'Hello! '
+            + req.params.name + "<br>"
+            + ((req.params.name == "ふらたん") ? "♡" : "")
+            + ((req.params.name == "はるふ") ? "はるふだよー" : "")
+        );
+    } else {
+        res.send("Hello! What's your name?");
+    }
 });
 
 module.exports = router;
